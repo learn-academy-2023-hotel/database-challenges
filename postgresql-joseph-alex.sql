965a914ee4f0301e13922e81a6e2f0ce92b4606f
alexnguyen <alexanderdnguyen4@gmail.com>
2023-11-08 15:51:00 -0800

    Finished challenges. Started stretch goals.

--- postgresql-joseph-alex.sql
+++ postgresql-joseph-alex.sql
@@ -59,7 +59,11 @@ LIMIT 15
 
 -- Which fifteen countries have the highest life expectancy? (HINT: starts with Andorra, ends with Spain)
 
-
+SELECT name, lifeexpectancy
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+ORDER BY lifeexpectancy DESC
+LIMIT 15
 
 -- Which five countries have the lowest population density (density = population / surfacearea)? (HINT: starts with Greenland)
 
@@ -108,13 +112,77 @@ LIMIT 1
 
 -- Who is the most influential head of state measured by population? (HINT: Jiang Zemin)
 
-
+SELECT name, population, headofstate
+FROM country
+ORDER BY population DESC
+LIMIT 1
 
 -- Of the countries with the top 10 gnp, which has the smallest population? (HINT: Canada)
+
+SELECT name, population, gnp
+FROM country
+ORDER BY gnp DESC
+LIMIT 10
+
 -- Of the 10 least populated countries with permanent residents (a non-zero population), which has the largest surfacearea? (HINT: Svalbard and Jan Mayen)
+
+SELECT name, population, surfacearea
+FROM country
+WHERE population > 0
+ORDER BY population ASC
+LIMIT 10
+
 -- Which region has the highest average gnp? (HINT: North America)
+
+SELECT region, AVG(gnp)
+FROM country
+GROUP BY region
+ORDER BY AVG DESC
+
 -- Who is the most influential head of state measured by surface area? (HINT: Elisabeth II)
+
+SELECT headofstate, SUM(surfacearea)
+FROM country
+GROUP BY headofstate
+ORDER BY SUM DESC
+
 -- What is the average life expectancy for all continents?
+
+SELECT continent, AVG(lifeexpectancy)
+FROM country
+GROUP BY continent
+
 -- What are the most common forms of government? (HINT: use count(*))
+
+SELECT governmentform, COUNT(*)
+FROM country
+GROUP BY governmentform
+ORDER BY COUNT DESC
+
 -- How many countries are in North America?
+
+SELECT continent, COUNT(name)
+FROM country
+GROUP BY continent
+ORDER BY COUNT DESC
+
 -- What is the total population of all continents?
+
+SELECT continent, SUM(population)
+FROM country
+GROUP BY continent
+ORDER BY SUM DESC
+
+
+-- 🏔 Stretch Goals
+
+-- Which countries have the letter "z" in the name? How many?
+
+
+
+-- Of the smallest 10 countries by area, which has the biggest gnp? (HINT: Macao)
+-- Of the smallest 10 countries by population, which has the biggest per capita gnp?
+-- Of the biggest 10 countries by area, which has the biggest gnp?
+-- Of the biggest 10 countries by population, which has the biggest per capita gnp?
+-- What is the sum of surface area of the 10 biggest countries in the world? The 10 smallest?
+-- What year is this country database from? Cross reference various pieces of information to determine the age of this database.
